3d22a4cbb39b0f3d5bb075497afb1c778e27e08c
Wallace Bescrovaine <wall.bescrovaine@hotmail.com>
2019-06-02 01:23:23 -0300

    EXGH

---
+++ src/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('../database');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    CreatedAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
